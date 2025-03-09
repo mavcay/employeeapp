01507b0a3629d068cdf9253704370777345903b8
mavcay <153567077+mavcay@users.noreply.github.com>
2025-03-09 08:56:24 +0800

    Update RetrofitClient.kt

--- app/src/main/java/com/gabriel/employeeapp/di/RetrofitClient.kt
+++ app/src/main/java/com/gabriel/employeeapp/di/RetrofitClient.kt
@@ -4,7 +4,7 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object RetrofitClient {
-    private const val BASE_URL = "http://192.168.0.173:8080/"
+    private const val BASE_URL = "http://192.168.X.XXX:8080/" //EDIT THE IP ADDRESS BASED ON YOUR IP ADDRESS, GO TO CMD AND ENTER IPCONFIG, THEN PASTE YOUR IP HERE, DONT CHANGE THE 8080
 
     val instance: Retrofit by lazy {
         Retrofit.Builder()
